6028ecf44e2aa5b51702b8ca08f650d380f87ba4
luizgust4v010 <github2810@gmail.com>
2025-03-21 22:16:43 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,47 +1,47 @@
 #include <iostream>
-#include <climits>
+
 using namespace std;
 
-int main() {
-    const int DIAS_ANO = 365;
-    double temperaturas[DIAS_ANO];
-    double soma = 0.0;
-    int diasAbaixoDaMedia = 0;
+int main()
+{
+    string  nome[3];
 
-
-    cout << "Digite a temperatura média de cada um dos " << DIAS_ANO << " dias do ano:" << endl;
-    for (int i = 0; i < DIAS_ANO; i++) {
-        cout << "Dia " << i + 1 << ": ";
-        cin >> temperaturas[i];
-        soma += temperaturas[i];
+    for (int i = 0; i < 3; i++)
+    {
+        cout << " digite o nome " << i + 1 << " : ";
+        cin >> nome[i];
     }
 
+    string nomeProcurado;
 
-    double mediaAnual = soma / DIAS_ANO;
+    cout << " digite o nome a ser procurado: ";
+    cin >> nomeProcurado;
 
+    bool encontrado = false;
 
-    double menorTemperatura = INT_MAX;
-    double maiorTemperatura = INT_MIN;
-
-
-    for (int i = 0; i < DIAS_ANO; i++) {
-        if (temperaturas[i] < menorTemperatura) {
-            menorTemperatura = temperaturas[i];
-        }
-        if (temperaturas[i] > maiorTemperatura) {
-            maiorTemperatura = temperaturas[i];
-        }
-        if (temperaturas[i] < mediaAnual) {
-            diasAbaixoDaMedia++;
+    for(int i = 0; i < 3; i++){
+        if (nome[i] == nomeProcurado){
+            encontrado = false;
+            break;
         }
     }
 
+    if (encontrado = false ){
+        cout << " nao achei " << endl;
+    }else {
+        cout << " achei " << endl;
 
-    cout << "\nResultados:" << endl;
-    cout << "Menor temperatura do ano: " << menorTemperatura << "°C" << endl;
-    cout << "Maior temperatura do ano: " << maiorTemperatura << "°C" << endl;
-    cout << "Temperatura média anual: " << mediaAnual << "°C" << endl;
-    cout << "Número de dias com temperatura inferior à média anual: " << diasAbaixoDaMedia << " dias" << endl;
-
+    }
     return 0;
+
+
+
+
+
+
+
+
+
+
+
 }
